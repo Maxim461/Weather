303b88ffefe6258df60bd804c4921e010fbd6bc0
Maxim461 <max2004lee@mail.ru>
2024-01-21 14:14:48 +0300

    Bigin made app Weather

---
+++ Weather/View/Main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by maxim on 20.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by maxim on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
